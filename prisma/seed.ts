3a1ba93a807909d83787e5db2f8de05881164918
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-11-04 12:58:09 -0300

    First Commit

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Julio Marques",
+      email: "juliomarquesjr@yahoo.com.br",
+      avatarUrl: "https://github.com/juliomarquesjr.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Exemplo Pool",
+      code: "BOL123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-03T19:00:00.072Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-04T11:30:30.072Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
